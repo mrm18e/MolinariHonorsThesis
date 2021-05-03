e1809c21b87779a6f439255d33a33c3d219dc5da
unknown <mehauer@fsu.edu>
2021-05-03 10:26:13 -0400

    Added a comment.

--- R/SANDBOX/DownloadCensusData.R
+++ R/SANDBOX/DownloadCensusData.R
@@ -1,2 +1,5 @@
+
+## This is how you download a file from a website. This will download Census Estimates for 2010-2019 by demographic
+## components of change for all counties. 
 download.file("https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv",
               "./R/DATA-RAW/co-est2019-alldata.csv")
