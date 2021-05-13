1e76d90ccfe3655395c57bcc76d57ffc00b1c7ab
mrm18e <mrmolinari@icloud.com>
2021-05-13 10:47:51 -0400

    Final Four Graphs in One Display

--- R/SANDBOX/DownloadCensusData.R
+++ R/SANDBOX/DownloadCensusData.R
@@ -1,16 +1,17 @@
 
-## This is how you download a file from a website. This will download Census Estimates for 2010-2019 by demographic
+## This is how you download a file from a website. This will download Census Estimates for 2010-2020 by demographic
 ## components of change for all counties. 
-download.file("https://www2.census.gov/programs-surveys/popest/datasets/2010-2019/counties/totals/co-est2019-alldata.csv",
-              "./R/DATA-RAW/co-est2019-alldata.csv")
+download.file("https://www2.census.gov/programs-surveys/popest/datasets/2010-2020/counties/totals/co-est2020-alldata.csv",
+              "./R/DATA-RAW/co-est2020-alldata.csv")
 
-
-dat <- read_csv("./R/DATA-RAW/co-est2019-alldata.csv") %>%
+library(tidyverse)
+dat <- read_csv("./R/DATA-RAW/co-est2020-alldata.csv") %>%
   mutate(GEOID = paste0(STATE, COUNTY)) %>%
   dplyr::select(STATE:CTYNAME,
-                POPESTIMATE2010:POPESTIMATE2019,
-                BIRTHS2010:DEATHS2019,
-                INTERNATIONALMIG2010:DOMESTICMIG2019)
+                POPESTIMATE2010:POPESTIMATE2020,
+                BIRTHS2010:DEATHS2020,
+                INTERNATIONALMIG2010:DOMESTICMIG2020)
 
 quantile(dat$INTERNATIONALMIG2016, 0.5)  
+
 
