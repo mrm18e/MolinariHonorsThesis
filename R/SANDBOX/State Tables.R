38c062eb211276ebd6415bc5576b954986d66cbd
mrm18e <mrmolinari@icloud.com>
2021-08-31 00:11:46 -0400

    States Table Finalized

--- R/SANDBOX/State Tables.R
+++ R/SANDBOX/State Tables.R
@@ -35,8 +35,8 @@ dat <- read_csv("./R/DATA-RAW/co-est2020-alldata.csv") %>%
 ## Making a Births dataset
 births <- dat %>%
   dplyr::select(GEOID:CTYNAME, # Selecting the ID variables
-                BIRTHS2010:BIRTHS2020) %>% # selecting only the births
-  pivot_longer(cols = c(BIRTHS2010:BIRTHS2020), # Need to go from wide to tall.
+                BIRTHS2019:BIRTHS2020) %>% # selecting only the births
+  pivot_longer(cols = c(BIRTHS2019:BIRTHS2020), # Need to go from wide to tall.
                names_to = "birthyears", # making the name "birthyears"
                values_to = "births") %>% # making the # of births called births
   mutate(year = substr(birthyears,7,11)) %>% # birthyears is "BIRTHS2010" so getting only the year.
@@ -51,3 +51,67 @@ births <- dat %>%
 
 births$perdrop[is.nan(births$perdrop)] <- NA # some values are 0/0 or 0/1 or 1/0. We set those to NA
 births[is.na(births)] <- 0 # we set all NA values to = 1.0
+
+
+
+## Making a Deaths dataset
+deaths <- dat %>%
+  dplyr::select(GEOID:CTYNAME,
+                DEATHS2019:DEATHS2020) %>%
+  pivot_longer(cols = c(DEATHS2019:DEATHS2020),
+               names_to = "deathyears",
+               values_to = "deaths") %>%
+  mutate(year = substr(deathyears,7,11)) %>%
+  filter(COUNTY != "000",
+         year != "2010") %>%
+  group_by(year, STATE, STNAME) %>% # Grouping by County, County name, and Year
+  summarise(deaths = sum(deaths)) %>%
+  group_by(STATE, STNAME) %>%
+  mutate(perdrop = (deaths - lag(deaths))/abs(lag(deaths))) %>%
+  # mutate(perdrop = if_else(lag(deaths)<0,abs(perdrop), perdrop)) %>%
+  I()
+
+deaths$perdrop[is.nan(deaths$perdrop)] <- NA # some values are 0/0 or 0/1 or 1/0. We set those to NA
+deaths[is.na(deaths)] <- 1 # we set all NA values to = 1.0
+
+
+## Making a Domestic Migrations dataset
+domesticmig <- dat %>%
+  dplyr::select(GEOID:CTYNAME,
+                DOMESTICMIG2019:DOMESTICMIG2020) %>%
+  pivot_longer(cols = c(DOMESTICMIG2019:DOMESTICMIG2020),
+               names_to = "domesticmigyears",
+               values_to = "domesticmig") %>%
+  mutate(year = substr(domesticmigyears,12,15)) %>%
+  filter(COUNTY != "000",
+         year != "2010") %>%
+  group_by(year, STATE, STNAME) %>% # Grouping by County, County name, and Year
+  summarise(domesticmig = sum(domesticmig)) %>%
+  group_by(STATE, STNAME) %>%
+  mutate(perdrop = (domesticmig - lag(domesticmig))/abs(lag(domesticmig))) %>%
+  # mutate(perdrop = if_else(lag(domesticmig)<0,abs(perdrop), perdrop)) %>%
+  I()
+
+domesticmig$perdrop[is.nan(domesticmig$perdrop)] <- NA # some values are 0/0 or 0/1 or 1/0. We set those to NA
+domesticmig[is.na(domesticmig)] <- 1 # we set all NA values to = 1.0
+
+
+## Making an International Migrations dataset
+internationalmig <- dat %>%
+  dplyr::select(GEOID:CTYNAME,
+                INTERNATIONALMIG2019:INTERNATIONALMIG2020) %>%
+  pivot_longer(cols = c(INTERNATIONALMIG2019:INTERNATIONALMIG2020),
+               names_to = "internationalmigyears",
+               values_to = "internationalmig") %>%
+  mutate(year = substr(internationalmigyears,17,20)) %>%
+  filter(COUNTY != "000",
+         year != "2010") %>%
+  group_by(year, STATE, STNAME) %>% # Grouping by County, County name, and Year
+  summarise(internationalmig = sum(internationalmig)) %>%
+  group_by(STATE, STNAME) %>%
+  mutate(perdrop = (internationalmig - lag(internationalmig))/abs(lag(internationalmig))) %>%
+  # mutate(perdrop = if_else(lag(internationalmig)<0,abs(perdrop), perdrop)) %>%
+  I()
+
+internationalmig$perdrop[is.nan(internationalmig$perdrop)] <- NA # some values are 0/0 or 0/1 or 1/0. We set those to NA
+internationalmig[is.na(internationalmig)] <- 1 # we set all NA values to = 1.0
